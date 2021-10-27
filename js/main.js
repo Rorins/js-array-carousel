76189953c32f2d1394648d5bee16924a40b0b86d
Aurora Grippaudo <77445911+Rorins@users.noreply.github.com>
2021-10-27 16:44:38 +0200

    Generato elementi con js

---
+++ js/main.js
@@ -0,0 +1,63 @@
+/**
+ * JS CAROUSEL
+ *
+ * Dati tre array contenenti
+ * una lista ordinata di 5 immagini e
+ * una lista ordinata dei relativi 5 luoghi,
+ * e una lista di 5 testi.
+ * creare un carosello come nella foto alegata
+ *
+ * Al click dell'utente sulle frecce l'immagine attiva diventa
+ * visibile in formato grande a sinistra e
+ * nel suo angolo in basso a destra dovranno essere aggiunti i relativi titolo e il testo.
+ * Aggiorniamo anche la thumbnail attiva.
+ */
+
+// Immagini
+const imageCollection = [
+    './img/01.jpg',
+    './img/02.jpg',
+    './img/03.jpg',
+    './img/04.jpg',
+    './img/05.jpg',
+];
+
+// Titoli
+const titleCollection = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise',
+];
+
+// Testi
+const textCollection = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
+
+/* Richiamo classe images creando una sua variabile*/
+const images = document.querySelector(".images")
+/* Richiamo classe images creando una sua variabile*/
+const thumbs = document.querySelector(".thumbs")
+
+/*Genero il contenuto di images usando un loop che andrà a prendere le immagini dalla relativa lista*/
+for (let i = 0; i<imageCollection.length; i++){
+    images.innerHTML += `<div class="image-container">
+    <img src=${imageCollection[i]} alt="${titleCollection[i]}">
+    <div class="text">
+        <h3>${titleCollection[i]}</h3>
+        <p>${textCollection[i]}</p>
+    </div>
+</div>`;
+
+/*Genero il contenuto di thumbs usando un loop che andrà a prendere le thumbs dalla relativa lista*/
+thumbs.innerHTML += `<div class="thumb active">
+<img src=${imageCollection[i]} alt="${titleCollection[i]}">
+</div>`
+}
+
